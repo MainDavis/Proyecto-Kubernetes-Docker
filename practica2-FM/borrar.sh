cd720fc0c918acaee868013df1000e9867fcb531
MainDavis <44068483+MainDavis@users.noreply.github.com>
2021-11-18 14:35:37 +0100

    Varios Pods y compartidos

--- practica2-FM/borrar.sh
+++ practica2-FM/borrar.sh
@@ -1,3 +1,6 @@
 kubectl delete deployment kubernetes-practica2fm
 kubectl delete service practica2fm-entrypoint
+
+sleep 5
+
 sudo docker image rm $(sudo docker images | grep -i "maindavis/filemanager_ubuntu" | cut -f17 -d " ")
