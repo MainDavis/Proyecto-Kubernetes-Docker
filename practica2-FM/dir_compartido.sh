da82cffd758a1aad7dc6028cd895f4456621d4b7
MainDavis <44068483+MainDavis@users.noreply.github.com>
2021-11-29 13:42:41 +0100

    Update dir_compartido.sh

--- practica2-FM/dir_compartido.sh
+++ practica2-FM/dir_compartido.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sshfs hugo@10.1.205.53:/home/hugo/Desktop/PSD1-PRACTICA-2/practica2-FM/dirServer /home/davis/Escritorio/GitHub/PSD1-PRACTICA-2/practica2-FM/dirServer -o nonempty
+sshfs davis@10.1.201.105:/home/davis/Escritorio/GitHub/PSD1-PRACTICA-2/practica2-FM/dirServer /home/hugo/Desktop/PSD1-PRACTICA-2/practica2-FM/dirServer
