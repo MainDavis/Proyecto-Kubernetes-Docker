c1c959ff284c55005524e96c47a7589733210535
MainDavis <44068483+MainDavis@users.noreply.github.com>
2021-11-29 14:41:18 +0100

    Cositas

--- practica2-FM/creacion.sh
+++ practica2-FM/creacion.sh
@@ -1,5 +1,5 @@
 sudo docker build .
-sudo docker tag $(sudo docker images | grep -i "<none>" | cut -f36 -d " ") maindavis/filemanager_ubuntu:1.0
+sudo docker tag $(sudo docker images | grep -i "<none>" | cut -f40 -d " ") maindavis/filemanager_ubuntu:1.0
 sudo docker push docker.io/maindavis/filemanager_ubuntu:1.0
 
 kubectl apply -f practica2_fm.yaml
